14eb8f92246fd48dd1b138c381339a34192818fb
Griffin Rades <ggrade16@smumn.edu>
2019-11-22 16:24:26 -0600

    working login

--- barApp/barApp/views/ViewController.swift
+++ barApp/barApp/views/ViewController.swift
@@ -8,21 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
 }
 
